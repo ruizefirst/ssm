fcb75f001ce3d4a54bc9c6b00d6d55c90a23c2cb
Apollo <ruizefirst@163.com>
2017-06-25 03:21:30 +0800

    ssm

--- free/include/config_passport.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$cfg_au_isopen = '1';
-$cfg_au_company = 'ViooMAÍøÂç';
-$cfg_au_name = "ViooMAÍøÂç ×ÉÑ¯QQ:<a href='http://wpa.qq.com/msgrd?V=1&Uin=475994986&Site=ioshenmue&M' target='_blank'>475994986</a>";
-$cfg_au_homepage = 'http://www.viooma.com';
-$cfg_au_bbsurl = 'http://bbs.viooma.com/';
-$cfg_au_regurl = 'http://bbs.viooma.com/register.php';
-$cfg_au_version = 'ViooMA½øÏú´æ 2008(WEB°æ)';
-?>
